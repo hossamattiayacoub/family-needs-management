e2aa364484825ad6d208ca5e46ce6416f8d2c5e1
hossamattiayacoub <hossamattia14@gmail.com>
2026-07-14 18:00:35 +0300

    evn prod edit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Replace with your deployed Apps Script Web App "exec" URL.
-  apiUrl: 'https://script.google.com/macros/s/REPLACE_WITH_YOUR_DEPLOYMENT_ID/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbycIIasYSCLTaUmZP_-TPv-8qDJ_mA08RJ93Nq0bFw4zuxnwIb7xHAgjmOhdWC7cOxM2w/exec'
 };
