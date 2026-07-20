dd3ebc04367cf851c671c6e822c92077d961e109
hossamattiayacoub <hossamattia14@gmail.com>
2026-07-20 10:56:07 +0300

    editapiurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Replace with your deployed Apps Script Web App "exec" URL.
-  apiUrl: 'https://script.google.com/macros/s/AKfycbzzbmgj2JzCLN11njP76Yzkx9O51jLNoOFQIQroQWav95pKWLWu41D4qWWgRU2zF78IUw/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzxwiixyJGm61MA685rZpcfe-JWHxMeaRponrwakZQab2-dAlXvLmCWwUVyrOjFp_gCWg/exec'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // Replace with your deployed Apps Script Web App "exec" URL.
   // Example: https://script.google.com/macros/s/AKfycbxXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/exec
-  apiUrl: 'https://script.google.com/macros/s/AKfycbzzbmgj2JzCLN11njP76Yzkx9O51jLNoOFQIQroQWav95pKWLWu41D4qWWgRU2zF78IUw/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzxwiixyJGm61MA685rZpcfe-JWHxMeaRponrwakZQab2-dAlXvLmCWwUVyrOjFp_gCWg/exec'
 };
